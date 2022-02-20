c935d0bc35c29f03c8a1024a768399a0ec34fd03
Geraldogrj <geraldogrj@gmail.com>
2022-02-19 23:00:53 -0300

    criação dos relacionamentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.empregados.models import Empregado
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, help_text="Descrição do Documento")
+    detentor = models.ForeignKey(Empregado, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.empregados.models import Empregado
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    empregado = models.ForeignKey(Empregado, on_delete=models.PROTECT)
 
 
     def __str__(self):
